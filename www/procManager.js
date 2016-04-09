96b1a9fab9fd7c3d1409d312a846391c41fa767e
liront <ltagger@gmail.com>
2016-04-09 12:03:00 +0300

    test

--- www/procManager.js
+++ www/procManager.js
@@ -1,10 +1,23 @@
 function procManager() {
   this.exec = function(cmd, callback) {
-    return cordova.exec(callback, function(err) {
-      callback({exitStatus: 100, output: err});
-    }, "procManager", "exec", [cmd]);
-
+    return cordova.exec(callback, failCB, 'ShellExec', 'exec', [cmd]);
   };
+
+  this.getProcessList = function(successCB, failCB) {
+    return cordova.exec(successCB, failCB, 'ShellExec', 'getProcessList', []);
+  };
+
+  this.killProcessByName = function(packageName, successCB, failCB) {
+    return cordova.exec(successCB, failCB, 'ShellExec', 'killProcessByName', [packageName]);
+  };
+
+  this.killAllNonSystemProcess = function(successCB, failCB) {
+    return cordova.exec(successCB, failCB, 'ShellExec', 'killAllNonSystemProcess', []);
+  };
+
+  function failCB(callback, err){
+    callback({exitStatus: 100, output: err});
+  }
 }
 
 window.procManager = new procManager();
